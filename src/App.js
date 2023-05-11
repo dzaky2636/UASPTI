99ce24f2cd5485c9768a4640ba00ffa1c191fd8a
Batara Hotdo <batarah07d0@gmail.com>
2023-05-11 12:48:36 +0700

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./component/Header";
 import "./style/App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="header">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
