24ca512d9804c504e4ce7a8b245310af50d2849b
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-05-16 13:28:46 +0700

    change font

---
+++ src/component/HeroPlay.jsx
@@ -0,0 +1,11 @@
+export function HeroPlay() {
+    return (
+        <div className="relative isolate px-6 pt-14 mt-20 lg:px-8">
+            <div class="px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-7xl">Let's Play</h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc convallis, ligula sit amet pulvinar pellentesque, ipsum ligula suscipit urna, et luctus leo velit nec ipsum. Suspendisse laoreet ultrices facilisis.</p>
+            </div>
+        </div>
+    );
+  }
+  
--- src/pages/PlayQuiz.js
+++ src/pages/PlayQuiz.js
@@ -1,10 +1,18 @@
 import { Navbar } from "../component/Navbar"
+import { HeroPlay } from "../component/HeroPlay"
+import { SelectPlayMode } from "../component/SelectPlayMode"
+
+const play = {
+    timer: 15,
+    poin: 0
+}
 
 export function PlayQuiz(){
     return(
         <div className="bg-white">
             <Navbar />
-            
+            <HeroPlay />
+            <SelectPlayMode />
         </div>
     )
 }
