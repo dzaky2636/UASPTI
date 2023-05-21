d80e57121a4a4c91de5b567a03ac34d8de4ffeee
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-05-21 09:24:26 +0700

    navbar reduction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./style/App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { PlayQuiz } from "./pages/PlayQuiz";
-import { HowTo } from "./pages/HowTo";
 import { Leaderboard } from "./pages/Leaderboard";
 import { AboutUs } from "./pages/AboutUs";
 
@@ -11,7 +10,6 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/play" element={<PlayQuiz />} />
-			<Route path="/howto" element={<HowTo />} />
 			<Route path="/leaderboard" element={<Leaderboard />} />
 			<Route path="/aboutus" element={<AboutUs />} />
 		</Routes>
