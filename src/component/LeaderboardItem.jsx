7f95043fc8317f8f29f355995a487b10b7d6fafd
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-05-23 08:42:03 +0700

    add leaderboard

---
+++ src/component/LeaderboardItem.jsx
@@ -0,0 +1,15 @@
+export function LeaderboardItem(props) {
+	return (
+		<div className="flex gap-4 bg-gradient-to-br from-[#4EA8DE] to-[#5E60CE] rounded-xl p-4 w-4/5 lg:w-1/3 mx-auto border-2 border-white">
+			<div className="text-lg lg:text-xl font-bold content-center my-auto text-white">
+				{props.no}
+			</div>
+			<div className="flex w-16 lg:w-20 h-16 lg:h-20 rounded-full border-4 border-white bg-slate-400">
+				<div className="text-4xl lg:text-5xl mx-auto my-auto">{props.icon}</div>
+			</div>
+			<div className="text-lg lg:text-xl font-bold my-auto text-white">
+				{props.nama}
+			</div>
+		</div>
+	);
+}
--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,18 +1,76 @@
 import { Navbar } from "../component/Navbar";
+import { LeaderboardItem } from "../component/LeaderboardItem";
+import { Footer } from "../component/Footer";
 
 export function Leaderboard() {
 	return (
 		<div>
 			<Navbar />
-			<div className="mx-2">
-				<div className="flex gap-2">
-					<div className="w-full lg:w-1/3 bg-cyan-600 rounded-lg mt-[5.5rem] p-20">
-						<h1 className="text-8xl font-bold">Ranking pemain</h1>
+			<div className="bg-gradient-to-b from-[#CAF0F8] to-[#48CAE4]">
+				<div className="container mx-auto">
+					<h1 className="text-3xl lg:text-4xl font-bold text-center pt-28 text-white">
+						Leaderboard
+					</h1>
+					<div className="flex gap-2 lg:gap-4 justify-center pt-8 text-white">
+						<button className="p-2 px-6 lg:px-12 rounded-full font-bold text-md lg:text-lg border-4 border-blue-800 bg-blue-500 hover:bg-indigo-500 transition ease-in-out hover:-translate-y-1 hover:scale-110">
+							Kategori
+						</button>
+						<button className="p-2 px-6 lg:px-12 rounded-full font-bold text-md lg:text-lg border-4 border-blue-800 bg-blue-500 hover:bg-indigo-500 transition ease-in-out hover:-translate-y-1 hover:scale-110">
+							Kategori
+						</button>
+						<button className="p-2 px-6 lg:px-12 rounded-full font-bold text-md lg:text-lg border-4 border-blue-800 bg-blue-500 hover:bg-indigo-500 transition ease-in-out hover:-translate-y-1 hover:scale-110">
+							Kategori
+						</button>
 					</div>
-					<div className="w-full lg:w-4/6 bg-cyan-600 rounded-lg mt-[5.5rem] p-20">
-						<h1 className="text-4xl font-bold">Lorem ipsum</h1>
+					<div className="flex gap-0 lg:gap-5 justify-center pt-10">
+						<div className="flex flex-col justify-center order-2 scale-100">
+							<div className="flex w-32 h-32 bg-slate-100 border-4 border-yellow-400 rounded-full ">
+								<div className="text-8xl pt-2">😎</div>
+							</div>
+							<div className="-mt-8">
+								<div className="w-12 h-12 p-2 mx-auto bg-yellow-400 border-4 border-yellow-400 rounded-full">
+									<div className="ps-[0.5rem] text-xl font-bold text-white">
+										1
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col justify-center order-1 scale-75">
+							<div className="w-32 h-32 bg-slate-100 border-4 border-[#afafaf] rounded-full ">
+								<div className="text-8xl px-2 py-2">🎨</div>
+							</div>
+							<div className="-mt-8">
+								<div className="w-12 h-12 p-2 mx-auto bg-[#afafaf]  border-4 border-[#afafaf]  rounded-full">
+									<div className="ps-[0.5rem] text-xl font-bold text-white">
+										2
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col justify-center order-3 scale-75">
+							<div className="w-32 h-32 bg-slate-100 border-4 border-[#aa895b] rounded-full ">
+								<div className="text-8xl px-2 py-2">💕</div>
+							</div>
+							<div className="-mt-8">
+								<div className="w-12 h-12 p-2 mx-auto bg-[#aa895b] border-4 border-[#aa895b] rounded-full">
+									<div className="ps-[0.5rem] text-xl font-bold text-white">
+										3
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="flex gap-4 flex-col pt-10">
+						<LeaderboardItem no="4" icon="👎" nama="John Thor" />
+						<LeaderboardItem no="5" icon="🐗" nama="John Thor" />
+						<LeaderboardItem no="6" icon="🤦‍♂️" nama="John Thor" />
+						<LeaderboardItem no="7" icon="🗾" nama="John Thor" />
+						<LeaderboardItem no="8" icon="🎶" nama="John Thor" />
+						<LeaderboardItem no="9" icon="😜" nama="John Thor" />
+						<LeaderboardItem no="10" icon="🔥" nama="John Thor" />
 					</div>
 				</div>
+				<Footer />
 			</div>
 		</div>
 	);
