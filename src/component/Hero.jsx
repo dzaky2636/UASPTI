cd5f2e96d90bfe5f8720aa7b4cf6f5d4015bf1ab
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-05-29 08:56:53 +0700

    FINAL. Update hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import bgherooverlay from "../assets/select mode bg/Movie.jpg";
 import "../style/index.css";
 
 const QuoteAPIurl = "https://type.fit/api/quotes";
@@ -29,15 +30,19 @@ export function Hero() {
 	};
 
 	return (
-		<div className="relative isolate px-6 pt-14 lg:px-8">
+		<div className="bgHero relative isolate px-6 pt-14 lg:px-8">
 			<div className="mx-auto max-w-2xl py-[10vh] sm:py-48 lg:py-[19vh] hover:scale-105 transition ease-in-out">
 				<div className="text-center">
 					<h1 className="text-[10vw] font-bold tracking-tight sm:text-6xl">
-						Hi, Welcome to Our Quiz Website
+						Welcome to <span className="text-yellow-400">UQuizz!</span>
 					</h1>
-					<p className="mt-6 text-[2vh] leading-5  para">
-						Don't worry about the score, just focus on your answer. In this
-						test, you able to know more knowledge.
+					<p className="mt-6 text-lg lg:text-2xl leading-5 para ">
+						Let's test your trivia knowledge, and see what's your score compared
+						to other people! How nerdy are you really?
+					</p>
+					<p className="mt-6 text-sm leading-5 para mx-14">"{quote.text}"</p>
+					<p className="mt-0 text-sm leading-5 para">
+						- {quote.author != null ? quote.author : "Unknown"}
 					</p>
 					<div className="mt-10 mb-[5vw] flex items-center justify-center gap-x-6">
 						<button
