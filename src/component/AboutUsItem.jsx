843292163f54eb04a566c7571fe9f0cb22205e5b
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-05-29 07:57:27 +0700

    ALMOST FINAL. (add quote api, fix responsive, bugfix)

--- src/component/AboutUsItem.jsx
+++ src/component/AboutUsItem.jsx
@@ -1,50 +1,45 @@
 export function AboutUsItem(props) {
-  return (
-    <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-      <div class="flex flex-col">
-        <a href="#" class="mx-auto">
-          <img
-            class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
-            src={props.foto}
-          ></img>
-        </a>
+	return (
+		<div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+			<div className="flex flex-col">
+				<a href="#" className="mx-auto">
+					<img
+						className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+						src={props.foto}></img>
+				</a>
 
-        <div class="text-center mt-6">
-          <h1 class="text-black text-3xl lg:text-xl font-bold mb-1">
-            {props.nama}
-          </h1>
-          <div class="text-black font-normal mb-2 text-xl lg:text-md">
-            ({props.nim})
-          </div>
-          <div className="flex justify-center">
-            <div
-              class="flex items-center justify-center opacity-85 hover:opacity-100
-                                transition-opacity duration-300 bg-white rounded-full w-[30%]"
-            >
-              <a
-                href={`${props.github}`}
-                class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-              >
-                <i class="mdi mdi-github text-indigo-500 mx-auto mt-2"></i>
-              </a>
+				<div className="text-center mt-6">
+					<h1 className="text-black text-3xl lg:text-xl font-bold mb-1">
+						{props.nama}
+					</h1>
+					<div className="text-black font-normal mb-2 text-xl lg:text-md">
+						({props.nim})
+					</div>
+					<div className="flex justify-center pt-2">
+						<div
+							className="flex items-center justify-center opacity-85 hover:opacity-100
+              transition-opacity duration-300 bg-white rounded-full w-[70%]">
+							<a
+								href={`${props.github}`}
+								className="flex rounded-full hover:bg-indigo-50 h-10 w-10">
+								<i className="mdi mdi-github text-indigo-500 mx-auto mt-2"></i>
+							</a>
 
-              <a
-                href={`${props.discord}`}
-                class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-              >
-                <i class="mdi mdi-discord text-[#0f1ba1] m-auto pl-[1px]"></i>
-              </a>
+							<a
+								href={`${props.discord}`}
+								className="flex rounded-full hover:bg-indigo-50 h-10 w-10">
+								<i className="mdi mdi-discord text-[#0f1ba1] m-auto pl-[1px]"></i>
+							</a>
 
-              <a
-                href={`${props.instagram}`}
-                class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
-              >
-                <i class="mdi mdi-instagram text-[#fd6caa] m-auto pl-[1px]"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							<a
+								href={`${props.instagram}`}
+								className="flex rounded-full hover:bg-indigo-50 h-10 w-10">
+								<i className="mdi mdi-instagram text-[#fd6caa] m-auto pl-[1px]"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,30 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "../style/index.css";
+
+const QuoteAPIurl = "https://type.fit/api/quotes";
+
 export function Hero() {
-  const handleClick = () => {
-    const targetElement = document.getElementById("HowTo");
-    targetElement.scrollIntoView({ behavior: "smooth" });
-  };
-  return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-[10vh] sm:py-48 lg:py-[19vh] hover:scale-105 transition ease-in-out">
-        <div className="text-center">
-          <h1 className="text-[10vw] font-bold tracking-tight sm:text-6xl">
-            Hi, Welcome to Our Quiz Website
-          </h1>
-          <p className="mt-6 text-[2vh] leading-5  para">
-            Don't worry about the score, just focus on your answer. In this
-            test, you able to know more knowledge.
-          </p>
-          <div className="mt-10 mb-[5vw] flex items-center justify-center gap-x-6">
-            <button
-              className="rounded-md bg-[#00787e] px-3.5 py-2.5 text-[1rem] font-semibold  shadow-sm hover:text-black hover:bg-[#fedf51]"
-              onClick={handleClick}
-            >
-              How to Play
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const [quote, getQuote] = useState([]);
+
+	const handleClick = () => {
+		const targetElement = document.getElementById("HowTo");
+		targetElement.scrollIntoView({ behavior: "smooth" });
+	};
+
+	useEffect(() => {
+		getAllQuote();
+	}, []);
+
+	const getAllQuote = () => {
+		axios
+			.get(QuoteAPIurl)
+			.then((response) => {
+				const quoteArr = response.data;
+				const randomIndex = Math.floor(Math.random() * quoteArr.length);
+				const quote = quoteArr[randomIndex];
+				getQuote(quote);
+			})
+			.catch((error) => console.error(`Error ${error}`));
+	};
+
+	return (
+		<div className="relative isolate px-6 pt-14 lg:px-8">
+			<div className="mx-auto max-w-2xl py-[10vh] sm:py-48 lg:py-[19vh] hover:scale-105 transition ease-in-out">
+				<div className="text-center">
+					<h1 className="text-[10vw] font-bold tracking-tight sm:text-6xl">
+						Hi, Welcome to Our Quiz Website
+					</h1>
+					<p className="mt-6 text-[2vh] leading-5  para">
+						Don't worry about the score, just focus on your answer. In this
+						test, you able to know more knowledge.
+					</p>
+					<div className="mt-10 mb-[5vw] flex items-center justify-center gap-x-6">
+						<button
+							className="rounded-md bg-[#00787e] px-3.5 py-2.5 text-[1rem] font-semibold  shadow-sm hover:text-black hover:bg-[#fedf51]"
+							onClick={handleClick}>
+							How to Play
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
