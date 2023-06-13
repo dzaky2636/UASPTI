c3a633a01548a57b0adf78ba1b59c260472da258
Dzaky Fatur Rahman <dzaky2636@gmail.com>
2023-06-13 16:22:31 +0700

    fix

--- src/component/QuizAnswerButton.jsx
+++ src/component/QuizAnswerButton.jsx
@@ -1,24 +1,22 @@
 export function QuizAnswerButton(props) {
- 
-  const { onClick, text, parameters } = props;
+	const { onClick, text, parameters } = props;
 
-  const handleClick = () => {
-    if (onClick) {
-      onClick(parameters);
-    }
-  };
+	const handleClick = () => {
+		if (onClick) {
+			onClick(parameters);
+		}
+	};
 
-  return (
-    <button
-      onClick={handleClick}
-      type="button"
-      className={`${props.order} choice-text  w-44 p-4 px-10 text-xl border-2 border-white rounded-full text-white bg-gradient-to-br from-blue-400 to-orange-500 via-purple-500 animate-gradient-x hover:bg-gradient-to-br hover:from-white hover:to-white hover:text-black hover:border-black hover:scale-105 transition ease-in-out`}
-      data-number={`${props.datanumber}`}
-      dangerouslySetInnerHTML={{
-        __html: props.answer,
-      }}
-    ></button>
-  );
+	return (
+		<button
+			onClick={handleClick}
+			type="button"
+			className={`${props.order} ${props.className} choice-text w-44 p-2 text-xl border-2 border-white rounded-3xl text-white bg-gradient-to-br from-blue-400 to-orange-500 via-purple-500 animate-gradient-x hover:scale-105 transition ease-in-out`}
+			data-number={`${props.datanumber}`}
+			dangerouslySetInnerHTML={{
+				__html: props.answer,
+			}}></button>
+	);
 
-  <QuizAnswerButton/>;
+	<QuizAnswerButton />;
 }
